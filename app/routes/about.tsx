5484a6a2f3a2ba197d11c47058075bc7a3afc693
palthol <ilikericeat@gmail.com>
2025-03-30 04:12:47 -0400

    about file changes

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,12 +1,13 @@
-import { SkillBadge } from "../components/SkillBadge";
-import profilePhoto from "../assets/profile-photo";
+import SkillBadge from "../components/SkillBadge";
+// Either:
+import profilePhotoSrc from "../assets/profile-photo"; // If it's an image file
 
-export function About() {
+export default function About() {
   return (
     <main className="flex flex-col items-center p-4">
       <h1 className="text-3xl font-bold mb-4">About Me</h1>
       <div className="flex flex-col items-center mb-8">
-        <img src={profilePhoto} alt="Profile" className="rounded-full w-32 h-32 mb-4" />
+        <img src={profilePhotoSrc} alt="Profile" className="rounded-full w-32 h-32 mb-4" />
         <p className="text-lg text-center">
           Hello! I'm a passionate web developer with experience in building dynamic and responsive web applications. I love creating user-friendly interfaces and exploring new technologies.
         </p>
